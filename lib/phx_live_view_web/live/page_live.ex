f231fc59e504f66ef156d49024ea42e51a1b4b86
MauricioMSoares <mauricio.soares@sptech.school>
2024-02-14 16:51:17 -0300

    feat: added a custom number form in page_live

--- lib/phx_live_view_web/live/page_live.ex
+++ lib/phx_live_view_web/live/page_live.ex
@@ -2,7 +2,7 @@ defmodule PhxLiveViewWeb.PageLive do
   use PhxLiveViewWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, number: 0)}
+    {:ok, assign(socket, number: 0, form: to_form(%{default_number: 0}))}
   end
 
   def render(assigns) do
@@ -12,6 +12,11 @@ defmodule PhxLiveViewWeb.PageLive do
     <br />
     <.button phx-click="add">Add</.button>
     <.button phx-click="subtract">Subtract</.button>
+    <br />
+    <.simple_form for={@form} phx-submit="add_number">
+      <.input type="number" field={@form[:custom_number]} value={@form.params.default_number} />
+      <.button>Add Custom Number</.button>
+    </.simple_form>
 
     <br />
     <.example_paragraph />
@@ -32,6 +37,15 @@ defmodule PhxLiveViewWeb.PageLive do
 
   def handle_event("subtract", _params, socket) do
     {:noreply, assign(socket, number: socket.assigns.number - 1)}
+  end
+
+  def handle_event("add_number", %{"custom_number" => custom_number}, socket) do
+    parsed_number = case Integer.parse(custom_number) do
+      {number, _} -> number
+      :error -> 0
+    end
+
+    {:noreply, assign(socket, number: socket.assigns.number + parsed_number, form: to_form(%{default_number: parsed_number}))}
   end
 
   def handle_event("navigate", _params, socket) do
